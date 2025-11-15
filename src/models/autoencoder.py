e4c3feecd7da09ec3905407e42789f894d00ad7b
dcr23 <dcr23@inf.ufpr.br>
2025-11-15 14:14:16 -0300

    add compression rate to autoencoder

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -7,45 +7,58 @@ and decoder can be instaces with diffents objects. Also
 will make fine tunning easier
 """
 
+
 class Encoder(nn.Module):
-    def __init__(self, input_dim=9860, latent_dim=1024):
+    def __init__(self, input_dim=9860, latent_dim=1024, compression_rate=0.5):
         super().__init__()
-        self.layers = nn.Sequential(
-                nn.Linear(input_dim, 4930),
-                nn.ReLU(), 
-                nn.Linear(4930, 2450),
-                nn.ReLU(), 
-                nn.Linear(2450, latent_dim)
-                )
+
+        self.layers = nn.Sequential()
+
+        current_dim = input_dim
+
+        while int(current_dim * compression_rate) > latent_dim:
+            next_dim = int(current_dim * compression_rate)
+            self.layers.append(nn.Linear(current_dim, next_dim))
+            self.layers.append(nn.ReLU())
+            current_dim = next_dim
+
+        self.layers.append(nn.Linear(current_dim, latent_dim))
 
     def forward(self, x):
         return self.layers(x)
+
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim=9860, latent_dim=1024):
+    def __init__(self, output_dim=9860, latent_dim=1024, decompression_rate=2):
         super().__init__()
-        self.layers = nn.Sequential(
-                nn.Linear(latent_dim, 2450),
-                nn.ReLU(),
-                nn.Linear(2450, 4930),
-                nn.ReLU(),
-                nn.Linear(4930, output_dim),
-                nn.Sigmoid() 
-                )
+
+        self.layers = nn.Sequential()
+
+        current_dim = latent_dim
+
+        while int(current_dim * decompression_rate) < output_dim:
+            next_dim = int(current_dim * decompression_rate)
+            self.layers.append(nn.Linear(current_dim, next_dim))
+            self.layers.append(nn.ReLU())
+            current_dim = next_dim
+
+        self.layers.append(nn.Linear(current_dim, output_dim))
+        self.layers.append(nn.Sigmoid())
 
     def forward(self, x):
         return self.layers(x)
 
+
 class UndercompleteAE(nn.Module):
-    def __init__(self, input_dim=9860, latent_dim=1024):
-        super().__init__()         
-        self.encoder = Encoder(input_dim, latent_dim)
-        self.decoder = Decoder(input_dim, latent_dim)
+    def __init__(self, input_dim=9860, latent_dim=1024, compression_rate=0.5):
+        super().__init__()
+        self.encoder = Encoder(input_dim, latent_dim, compression_rate)
+        self.decoder = Decoder(input_dim, latent_dim, 1 / compression_rate)
 
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
 
-    def export(self, path: str= "./UCAE_state") -> None:
+    def export(self, path: str = "./UCAE_state") -> None:
         torch.save(self.state_dict(), path)
